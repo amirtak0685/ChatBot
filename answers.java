0578607a0ecdfe8a8ef951ae372a173827cff6e0
Amir Takian <amirtak06@gmail.com>
2025-09-01 13:44:18 +0100

    Class used to store user's answers in a file

---
+++ answers.java
@@ -0,0 +1,9 @@
+class answers {
+    String name;
+    String[] knownAnswers;
+    String[] questions;
+    String[] actorQuestions;
+    String[] sceneQuestions;
+    String[] villainQuestions;
+    boolean asked;
+}
